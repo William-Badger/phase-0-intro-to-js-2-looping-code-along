c10f7a4973e374dae9379ce70a912e4e396a1fad
William Badger <wkbadger@gmail.com>
2021-05-26 17:25:40 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const surprise = ['surprise'];
+const guests = ['Lisa', 'Kaitlin', 'Jan'];
+function writeCards(guests, surprise) {
+    let message = []
+    for (let a = 0; a < guests.length; a++) {
+        console.log(message.push(`Thank you, ${guests[a]}, for the wonderful ${surprise} gift!`));
+    }
+    return message;
+}
+
+function countDown() {
+for (let countDown = 10; countDown >= 0; countDown--) {
+    console.log(countDown);
+}
+}
